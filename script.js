da2b313c9710a426031c090ebf107529b3c3da84
Rehan <imthatniga6969@gmail.com>
2026-07-08 16:25:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,83 @@
-//event bubbling and event capturing
+// let username = "Rehan";
 
+// if (username.length !== 0) {
+//     console.log(`Welcome ${username}`)
+// } else {
+//     console.log("Username not found")
+// }
 
-let parentEl = document.querySelector("#parent");
-let childEl = document.querySelector("#child");
+// 2. Movie Ticket Price
 
-parentEl.addEventListener("click", (event) => {
-    alert("Parent Element has been clicked")
-    event.stopPropagation()
-})
+// A movie theater charges:
 
+// Under 12 → ₹150
+// 12–59 → ₹250
+// 60 and above → ₹180
 
-childEl.addEventListener("click", (event) => {
-    alert("Child Element has been clicked")
-    event.stopPropagation()
+// Given:
+// let age = 65;
 
-})
+// Print the correct ticket price.
+// Practice: if...else if
+
+// let age = 23;
+
+// if (age>0 && age < 12) {
+// console.log("Your ticket price is ₹150");}
+// else if (age>= 12 && age <= 59) {
+// console.log("Your ticket price is ₹250");}
+// else if (age >= 60) {
+//     console.log("Your ticket price is ₹180");
+// } else {
+//     console.log("Enter valid age")
+// }
+
+// 3. Shopping Discount
+
+// A customer buys items worth:
+
+// let total = 3200;
+
+// Rules:
+// Above ₹5000 → 20% discount
+// Above ₹2000 → 10% discount
+// Otherwise → No discount
+
+// Print the final amount.
+
+// Practice: Conditions + arithmetic
+
+// let total = 3200;
+
+// if (total > 5000) {
+//   console.log("Above ₹5000 → 20% discount");
+// } else if (total > 2000) {
+//   console.log("Above ₹2000 → 10% discount");
+// } else {
+//   console.log("Otherwise → No discount");
+// }
+
+// 4. Password Strength Checker
+
+// Given:
+
+// let password = "hello123";
+
+// Rules:
+
+// Less than 8 characters → Weak
+// Contains at least 8 characters → Strong
+
+// Bonus: Check whether it contains a number.
+// Practice: Strings
+
+let password = "hello123";
+console.log(typeof password);
+if (password.length < 8) {
+    console.log("Weak Password")
+}
+else if (password.length >=8)
+{
+    console.log("strong password")
+}
+else if( password(typeof()))
